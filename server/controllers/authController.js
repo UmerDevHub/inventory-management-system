c885b0cb786c9190d89db631ce6536d2d9b5c1a9
Umer <umernisar053@gmail.com>
2026-08-02 00:28:24 +0500

    Add login endpoint in authController and authRoutes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,48 @@ const bcrypt = require("bcrypt");
 const generateToken = require("../utils/generateToken");
 
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please fill both fields",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      return res.status(200).json({
+        message: "User logged in successfully",
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+
 const registerUser = async (req, res) => {
   try {
 
@@ -53,4 +95,5 @@ const registerUser = async (req, res) => {
 
 module.exports = {
   registerUser,
+  loginUser,
 };
