70e1624180ff9159a44f4124dcc96ca1fd3ca912
Umer <umernisar053@gmail.com>
2026-08-01 23:41:59 +0500

    Implement user registration authentication with JWT

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
