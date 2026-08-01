dc69a976f8dbe4f1bbf8b0466da040bfb3d50268
Umer <umernisar053@gmail.com>
2026-08-01 22:43:06 +0500

    Add jsconfig.json and ensure consistent model path casing

---
+++ server/controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const generateToken = require("../utils/generateToken");
+
+
+const registerUser = async (req, res) => {
+  try {
+
+    const { name, email, password } = req.body;
+
+    // Check if all fields are provided
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all fields",
+      });
+    }
+
+    
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+  
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    
+    res.status(201).json({
+      message: "User registered successfully",
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
